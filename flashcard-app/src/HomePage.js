d9e625bff2fae0c9d6c6affcfe2b96ae61041d90
AmalMammadzada <mammadazda.amal05@gmail.com>
2023-12-18 19:05:02 +0400

    Home Page

---
+++ flashcard-app/src/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import projects from './projectsData';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <section className="introduction">
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is a general introduction to your portfolio.</p>
+      </section>
+
+      <section className="projects">
+        <h2>My Projects</h2>
+        {projects.map((project) => (
+          <div key={project.id} className="project">
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              Visit Project
+            </a>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
