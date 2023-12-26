d9a77bc85340018631dcf7cce0b8221f32922f5b
AmalMammadzada <mammadazda.amal05@gmail.com>
2023-12-26 23:02:23 +0400

    last modification date/time

--- flashcard-app/src/FlashCard.js
+++ flashcard-app/src/FlashCard.js
@@ -1,6 +1,8 @@
+// FlashCard.js
+
 import React, { useState } from 'react';
 
-const FlashCard = ({ question, answer }) => {
+const FlashCard = ({ question, answer, lastModified }) => {
   const [showAnswer, setShowAnswer] = useState(false);
 
   const toggleCard = () => {
@@ -12,10 +14,11 @@ const FlashCard = ({ question, answer }) => {
       <div className={`card-inner ${showAnswer ? 'show-answer' : ''}`}>
         <div className="card-front">
           <p>{question}</p>
-          {/* Additional content for the front side of the cardd */}
+          {/* Additional content for the front side of the card (e.g., images) */}
         </div>
         <div className="card-back">
           <p>{answer}</p>
+          <p>Last Modified: {lastModified}</p>
           {/* Additional content for the back side of the card */}
         </div>
       </div>
--- flashcard-app/src/FlashCardsPage.js
+++ flashcard-app/src/FlashCardsPage.js
@@ -1,32 +1,53 @@
+// FlashCardsPage.js
 
 import React from 'react';
 import FlashCard from './FlashCard';
 
 const FlashCardsPage = () => {
-  // Sample flash card data
-  const flashCardsData = [
+  const [flashCards, setFlashCards] = React.useState([
     {
       id: 1,
       question: 'What is React?',
-      answer: 'React is a JavaScript library',
-    },
-    {
-      id: 2,
-      question: 'What is JSX?',
-      answer: 'JSX is a syntax extension',
+      answer: 'React is a JavaScript library for building user interfaces.',
+      lastModified: new Date().toLocaleString(), // Initialize with current date/time
     },
     // Add more flash card objects as needed
-  ];
+  ]);
+
+  // Function to add/update a flash card
+  const handleAddOrUpdateCard = (cardData) => {
+    const existingCardIndex = flashCards.findIndex((card) => card.id === cardData.id);
+    if (existingCardIndex !== -1) {
+      // Card exists, update it
+      const updatedCards = [...flashCards];
+      updatedCards[existingCardIndex] = {
+        ...cardData,
+        lastModified: new Date().toLocaleString(), // Update lastModified
+      };
+      setFlashCards(updatedCards);
+    } else {
+      // Card doesn't exist, add it
+      setFlashCards([
+        ...flashCards,
+        {
+          ...cardData,
+          id: flashCards.length + 1, // Generate a new ID (replace with your logic)
+          lastModified: new Date().toLocaleString(), // Update lastModified
+        },
+      ]);
+    }
+  };
 
   return (
     <div className="flash-cards-page">
       <h2>Flash Cards</h2>
       <div className="flash-cards-container">
-        {flashCardsData.map((card) => (
+        {flashCards.map((card) => (
           <FlashCard
             key={card.id}
             question={card.question}
             answer={card.answer}
+            lastModified={card.lastModified}
           />
         ))}
       </div>
